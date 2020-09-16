ea85e80c2528edfe80066018bc075f63e1b5abea
Jan Altensen <info@stricted.net>
2020-09-16 04:27:03 +0200

    liber: libshims: add shim for android_memset32
    * memset32.S and android_memset.S taken from Q
    
    Change-Id: Iaf59389e32b87d181f6dccc34e9174da25171f4f

--- extract-files.sh
+++ extract-files.sh
@@ -26,5 +26,10 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 ANDROID_ROOT="$MY_DIR"/../../..
 BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
-CAMERA_HAL="$BLOB_ROOT"/vendor/lib64/hw/com.qti.chi.override.so
-sed -i "s/libhidltransport.so/qtimutex.so\x00\x00\x00\x00\x00\x00\x00\x00/" "$CAMERA_HAL"
+CAMERA_HAL_CHI="$BLOB_ROOT"/vendor/lib64/hw/com.qti.chi.override.so
+sed -i "s/libhidltransport.so/qtimutex.so\x00\x00\x00\x00\x00\x00\x00\x00/" "$CAMERA_HAL_CHI"
+
+CHARGE_ONLY="$BLOB_ROOT"/vendor/bin/charge_only_mode
+for LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "$CHARGE_ONLY"); do
+    patchelf --add-needed "libmemset_shim.so" "$LIBMEMSET_SHIM"
+done
